3b205f82564b2da4359cea3a90302a75fd14ecb1
rakibulGDH <rakibul@gtctrust.com>
2025-05-02 11:39:18 +0600

    flag added

---
+++ src/Home/Flag.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Flag = () => {
+    return (
+        <div>
+            <div className="flex justify-center items-center ">
+                <div className="bg-green-500 w-[500px] h-[300px] flex items-center justify-center rounded-md">
+                    <div className="bg-red-500 rounded-full w-[200px] h-[200px]">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Flag;
